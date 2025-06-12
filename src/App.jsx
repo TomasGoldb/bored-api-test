505088b65db817acafc3a975d9cb76656a81326c
Android Studio <devandroid@ort>
2025-06-12 08:20:39 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [idea, setIdea] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    generarAleatorio()
+  }, [])
+
+  const generarAleatorio = () => {
+    setLoading(true)
+    axios.get('https://api.adviceslip.com/advice')
+      .then(response => {
+        setIdea(response.data)
+        setLoading(false)
+      })
+      .catch(error => {
+        console.error(error)
+        setLoading(false)
+      })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <h1>Consejitos</h1>
+
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+          <p className="loading-text">Cargando consejo sabio...</p>
+        </div>
+      ) : (
+        <p className="advice">"{idea.slip.advice}"</p>
+      )}
+
+      <button className="btn" onClick={generarAleatorio}>Generar nuevo</button>
+    </div>
   )
 }
 
